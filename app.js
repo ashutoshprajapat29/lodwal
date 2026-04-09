fdb5f963f90484d5d882aa7ea6d06d7d456ae2ac
Ashutosh Prajapat <ashutoshprajapat2932005@gmail.com>
2026-04-09 15:27:51 +0530

    fix app.js and internal server isuues

--- app.js
+++ app.js
@@ -1,18 +1,24 @@
 const express = require('express');
 const path = require('path');
-const ejsMate = require('ejs-mate'); // Add this
+const ejsMate = require('ejs-mate');
 const app = express();
 
-app.engine('ejs', ejsMate); // Set engine
+app.engine('ejs', ejsMate);
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
 
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/', (req, res) => {
-    res.render('index'); // Renders index.ejs using the boilerplate
+    res.render('index');
 });
 
-app.listen(3000, () => {
-    console.log('Lodwal Construction site running on port 3000');
-});
+
+if (process.env.NODE_ENV !== 'production') {
+    app.listen(3000, () => {
+        console.log('Lodwal Construction site running on port 3000');
+    });
+}
+
+
+module.exports = app;
